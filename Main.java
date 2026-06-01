b161f5124315db55b966c832edafb84f61634ec9
Esfaa <anakke1rafa@gmail.com>
2026-06-01 10:17:01 +0800

    added small calculator

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int result = add(num1, num2);
+        System.out.println("The sum is: " + result);    
     }
+
+
+public static int add(int a, int b) {
+    return a + b;
+
+
+}
 }
